76f801213114ace93e386112d1579227f3aeae2c
Jennifer Sanders <jennys1000@gmail.com>
2020-11-03 23:50:18 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -10,7 +10,7 @@ function ageChecker(age) { if (age >=13 && age <=19) {return "You are a teenager
 
 }
 
-function ternaryTeenager(age) return (age>=13 && age <=19) ? return "You are a teenager" : return "You are not a teenager"
+function ternaryTeenager(age) {return (age>=13 && age <=19) ? return "You are a teenager" : return "You are not a teenager"}
 
 }
 
